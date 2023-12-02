a9e5316a776bb61d647e5ff78172a3486d0f0008
Rogier Roex <12000409@student.pxl.be>
2023-12-02 17:25:18 +0100

    day 2

---
+++ src/main/java/nl/rrx/day2/SnowIslandGame.java
@@ -0,0 +1,72 @@
+package nl.rrx.day2;
+
+import nl.rrx.FileUtil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SnowIslandGame {
+
+    private static List<BagGrab> ruleBook;
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+
+        List<String> games = FileUtil.readFile("day2-1");
+
+        setRules();
+
+        int snowIslandGameOutcome = playSnowIslandGame(games);
+        System.out.println(snowIslandGameOutcome);
+    }
+
+    private static int playSnowIslandGame(List<String> gameLines) {
+        int sumOfPossibleGameIDs = 0;
+
+        for (var line : gameLines) {
+            var splitLine = line.split(": ");
+            var bagGrabs = mapToBagGrabs(splitLine[1]);
+            if (isGamePossible(bagGrabs)) {
+                var gameID = Integer.parseInt(splitLine[0].replace("Game ", ""));
+                sumOfPossibleGameIDs += gameID;
+            }
+        }
+        return sumOfPossibleGameIDs;
+    }
+
+    private static List<BagGrab> mapToBagGrabs(String game) {
+        List<BagGrab> bagGrabs = new ArrayList<>();
+
+        var allGrabs = game.split("; |, ");
+        for (var grab : allGrabs) {
+            var splitAmountAndColor = grab.split(" ");
+            var amount = Integer.parseInt(splitAmountAndColor[0]);
+            var color = splitAmountAndColor[1];
+            bagGrabs.add(new BagGrab(amount, color));
+        }
+        return bagGrabs;
+    }
+
+    private static boolean isGamePossible(List<BagGrab> bagGrabs) {
+        for (var bagGrab : bagGrabs) {
+            var rule = ruleBook.stream()
+                    .filter(grabRule -> bagGrab.color.equals(grabRule.color))
+                    .findFirst()
+                    .orElseThrow();
+            if (bagGrab.amount > rule.amount) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void setRules() {
+        ruleBook = List.of(
+                new BagGrab(12, "red"),
+                new BagGrab(13, "green"),
+                new BagGrab(14, "blue")
+        );
+    }
+    record BagGrab(int amount, String color) {}
+}
---
+++ src/main/java/nl/rrx/day2/UpdatedSnowIslandGame.java
@@ -0,0 +1,55 @@
+package nl.rrx.day2;
+
+import nl.rrx.FileUtil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UpdatedSnowIslandGame {
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+
+        List<String> games = FileUtil.readFile("day2-1");
+
+        int gameOutcome = playSnowIslandGame(games);
+        System.out.println(gameOutcome);
+    }
+
+    private static int playSnowIslandGame(List<String> gameLines) {
+        int totalSnowIslandGamePower = 0;
+        for (var line : gameLines) {
+            var splitLine = line.split(": ");
+            var grabs = mapToBagGrabs(splitLine[1]);
+            totalSnowIslandGamePower += computeSnowIslandGamePower(grabs);
+        }
+        return totalSnowIslandGamePower;
+    }
+
+    private static int computeSnowIslandGamePower(List<BagGrab> grabs) {
+        var minimumAmounts = grabs.stream()
+                .collect(Collectors.toMap(
+                        BagGrab::color,
+                        BagGrab::amount,
+                        (amount1, amount2) -> amount1 > amount2 ? amount1 : amount2
+                ));
+        return minimumAmounts.get("red") * minimumAmounts.get("green") * minimumAmounts.get("blue");
+    }
+
+    private static List<BagGrab> mapToBagGrabs(String game) {
+        List<BagGrab> bagGrabs = new ArrayList<>();
+
+        var allGrabs = game.split("; |, ");
+        for (var grab : allGrabs) {
+            var splitGrab = grab.split(" ");
+            var amount = Integer.parseInt(splitGrab[0]);
+            var color = splitGrab[1];
+            bagGrabs.add(new BagGrab(amount, color));
+        }
+        return bagGrabs;
+    }
+
+    record BagGrab(int amount, String color) {}
+}

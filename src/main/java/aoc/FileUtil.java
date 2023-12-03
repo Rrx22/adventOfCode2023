2a7f1c794391a7741b70e41f5c4df4eb03eb7b22
Rogier Roex <12000409@student.pxl.be>
2023-12-03 11:40:15 +0100

    cleanup resource folder

--- src/main/java/aoc/FileUtil.java
+++ src/main/java/aoc/FileUtil.java
@@ -11,7 +11,7 @@ public class FileUtil {
     }
 
     public static List<String> readFile(String fileName) throws URISyntaxException, IOException {
-            var uri = ClassLoader.getSystemResource("realDeal/").toURI();
+            var uri = ClassLoader.getSystemResource("").toURI();
             var mainPath = Paths.get(uri).toString();
             var path = Paths.get(mainPath, fileName);
             return Files.readAllLines(path);

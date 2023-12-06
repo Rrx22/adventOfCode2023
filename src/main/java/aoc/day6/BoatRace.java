6d7ee5ef22b21365b59e71e32df38b6af71d1bbd
Rogier Roex <rogier.roex@gmail.com>
2023-12-06 13:37:21 +0100

    day 6

--- src/main/java/aoc/day6/BoatRace.java
+++ src/main/java/aoc/day6/BoatRace.java
@@ -4,42 +4,42 @@ import aoc.FileUtil;
 
 import java.util.Arrays;
 
+import static java.lang.StringTemplate.STR;
+
 public class BoatRace {
 
     public static void main(String[] args) {
         var input = FileUtil.readFile("day6");
         var times = input.get(0).replace("Time:", "").trim().split("\\s+");
         var distances = input.get(1).replace("Distance:", "").trim().split("\\s+");
-        System.out.println("----------------");
         System.out.println("Race options using poorly kerned input: " + raceWithPoorKerning(times, distances));
         System.out.println("----------------");
         System.out.println("Race options using correct input      : " + race(times, distances));
-        System.out.println("----------------");
     }
 
-    private static int race(String[] timeStrings, String[] distanceStrings) {
+    private static long race(String[] timeStrings, String[] distanceStrings) {
         var time = String.join("", timeStrings);
         var distances = String.join("", distanceStrings);
         return computeOptionsFor(Long.parseLong(time), Long.parseLong(distances));
     }
 
-    private static int raceWithPoorKerning(String[] timeStrings, String[] distanceStrings) {
+    private static long raceWithPoorKerning(String[] timeStrings, String[] distanceStrings) {
         var times = Arrays.stream(timeStrings).map(Integer::parseInt).toList();
         var distances = Arrays.stream(distanceStrings).map(Integer::parseInt).toList();
-        int result = 1;
+        long result = 1;
         for (int i = 0; i < times.size(); i++) {
             result *= computeOptionsFor(times.get(i), distances.get(i));
         }
         return result;
     }
 
-    private static int computeOptionsFor(long raceTime, long distanceToWin) {
+    private static long computeOptionsFor(long raceTime, long distanceToWin) {
         for (int buttonPressedTime = 0; buttonPressedTime < raceTime; buttonPressedTime++) {
             long leftOverRaceTime = raceTime - buttonPressedTime;
             long distanceWhenPressed = buttonPressedTime * leftOverRaceTime;
             if (distanceWhenPressed > distanceToWin) {
-                int numberOfOptions = (int) (leftOverRaceTime - buttonPressedTime) + 1;
-                System.out.printf("Time %d has %d options (Press button anywhere from %d to %d milliseconds!)%n", raceTime, numberOfOptions, buttonPressedTime, leftOverRaceTime);
+                long numberOfOptions = leftOverRaceTime - buttonPressedTime + 1;
+                System.out.println(STR."Time \{raceTime} has \{numberOfOptions} options (Press button anywhere from \{buttonPressedTime} to \{leftOverRaceTime} milliSeconds!)");
                 return numberOfOptions;
             }
         }

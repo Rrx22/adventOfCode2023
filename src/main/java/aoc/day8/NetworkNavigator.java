74e59639666734092d1dff0c42bc5e0ff41fdfb7
Rogier Roex <rogier.roex@gmail.com>
2023-12-08 17:37:41 +0100

    day 8

--- src/main/java/aoc/day8/NetworkNavigator.java
+++ src/main/java/aoc/day8/NetworkNavigator.java
@@ -2,70 +2,59 @@ package aoc.day8;
 
 import aoc.FileUtil;
 
-import java.util.ArrayList;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class NetworkNavigator {
 
+    private static char[] leftRight;
+    private static Map<String, String[]> allNodes;
+
     public static void main(String[] args) {
         var maps = FileUtil.readFile("day8");
-//        System.out.println("Traveled steps: " + navigateNetwork(maps));
-        System.out.println("Ghost steps: " + navigateGhostNetwork(maps));
+        analyzeMaps(maps);
+        System.out.printf("Traveled steps: %,d%n", navigateNetwork());
+        System.out.printf("Ghost steps: %,d%n", navigateGhostNetwork());
     }
 
-    private static int navigateGhostNetwork(List<String> maps) {
-        var leftRight = maps.get(0).toCharArray();
-        var nodes = mapNodes(maps);
-        int ghostSteps = 0;
-        int i = 0;
-        var nextNodes = nodes.keySet().stream()
+    private static int navigateNetwork() {
+        return countStepsForNode("AAA", "ZZZ");
+    }
+
+    private static BigInteger navigateGhostNetwork() {
+        return allNodes.keySet().stream()
                 .filter(k -> k.endsWith("A"))
-                .toList();
-        boolean done = false;
-        while (!done) {
-            var instruction = leftRight[i];
-            i = (i == leftRight.length - 1) ? 0 : i + 1;
-            List<String> foundNodes = new ArrayList<>();
-            for (var nextNode : nextNodes) {
-                foundNodes.add(nodes.get(nextNode)[(instruction == 'L') ? 0 : 1]);
-            }
-            ghostSteps++;
-            String log = ghostSteps + ": [" + String.join(", ", nextNodes) + "] -> " + String.join(", ", foundNodes) + "]";
-            nextNodes = foundNodes;
-            System.out.println(log);
-            done = nextNodes.stream().allMatch(s -> s.endsWith("Z"));
-        }
-        return ghostSteps;
+                .map(n -> BigInteger.valueOf(countStepsForNode(n, "Z")))
+                .reduce(BigInteger.ONE, (lcm, steps) -> lcm
+                        .multiply(steps)  // calculate the product (lcm * number)
+                        .divide(lcm.gcd(steps))); //divide by greatest common divider of lcm & number
     }
 
-    private static int navigateNetwork(List<String> maps) {
-        var leftRight = maps.get(0).toCharArray();
-        var nodes = mapNodes(maps);
+    private static int countStepsForNode(String node, String endsWith) {
         int steps = 0;
         int i = 0;
-        String nextNode = "AAA";
-        while (!nextNode.equals("ZZZ")) {
+        String nextNode = node;
+        while (!nextNode.endsWith(endsWith)) {
             var instruction = leftRight[i];
-            i = (i == leftRight.length - 1) ? 0 : i + 1;
+            nextNode = allNodes.get(nextNode)[(instruction == 'L') ? 0 : 1];
             steps++;
-            nextNode = nodes.get(nextNode)[(instruction == 'L') ? 0 : 1];
+            i = (i == leftRight.length - 1) ? 0 : i + 1;
         }
+        System.out.println(STR. "\{ steps } = (\{ node }, \{ nextNode })" );
         return steps;
     }
 
-    private static Map<String, String[]> mapNodes(List<String> maps) {
-        Map<String, String[]> nodes = new HashMap<>();
-        for (int i = 2; i < maps.size(); i++) {
-            String s = maps.get(i);
-            String loc = s.substring(0, s.indexOf("=")).trim();
-            String left = s.substring(s.indexOf("(") + 1, s.indexOf(",")).trim();
-            String right = s.substring(s.indexOf(",") + 1, s.indexOf(")")).trim();
-            nodes.put(loc, new String[]{left, right});
+    private static void analyzeMaps(List<String> maps) {
+        allNodes = new HashMap<>();
+        leftRight = maps.remove(0).toCharArray();
+        maps.remove(0);
+        for (var node : maps) {
+            var location = node.substring(0, 3);
+            var left = node.substring(7, 10);
+            var right = node.substring(12, 15);
+            allNodes.put(location, new String[]{left, right});
         }
-        return nodes;
     }
-
-    record Node(String loc, String left, String right) {}
 }

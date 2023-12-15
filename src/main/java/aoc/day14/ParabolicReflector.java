9e50ea0de40c04837da759838c6cde5969957992
Rogier Roex <rogier.roex@gmail.com>
2023-12-15 03:38:34 +0100

    day 14

--- src/main/java/aoc/day14/ParabolicReflector.java
+++ src/main/java/aoc/day14/ParabolicReflector.java
@@ -1,43 +1,53 @@
 package aoc.day14;
 
+import aoc.ChristmasAssert;
 import aoc.FileUtil;
 
 public class ParabolicReflector {
 
+    private static boolean log = false;
 
-    public static final char SQUARE_STONE = '#';
-    public static final char ROLLING_STONE = 'O';
-    public static final char EMPTY = '.';
+    private static final char SQUARE_STONE = '#';
+    private static final char ROLLING_STONE = 'O';
+    private static final char EMPTY = '.';
+
+    private static int height;
+    private static int width;
+    private static char[][] rockField;
 
     public static void main(String[] args) {
+        rockField = FileUtil.readToGrid("day14");
+        height = rockField.length;
+        width = rockField[0].length;
 
-        var input = FileUtil.readToGrid("day14");
-        tiltNorth(input);
-        analyze(input);
-
-
+        for (int i = 0; i < 1000000000; i++) {
+            tiltNorth();
+            tiltWest();
+            tiltSouth();
+            tiltEast();
+            if (i % 10000 == 0) System.out.printf("cycle %,d - TOTAL: %d%n", i, analyze());
+        }
+        ChristmasAssert.test(analyze(), 106689L);
     }
 
-    private static void tiltNorth(char[][] input) {
-        int height = input.length;
-        int width = input[0].length;
+    private static void tiltNorth() {
 
         for (int col = 0; col < width; col++) {
             var start = 0;
             int rollingRocks = 0;
             for (int row = 0; row < height; row++) {
-                var curr = input[row][col];
+                var curr = rockField[row][col];
                 if (curr == ROLLING_STONE) {
                     rollingRocks++;
                 }
-                if (curr == SQUARE_STONE || row == height-1) {
+                if (curr == SQUARE_STONE || row == height - 1) {
                     for (int i = start; i <= row; i++) {
-                        if (input[i][col] == SQUARE_STONE) continue;
+                        if (rockField[i][col] == SQUARE_STONE) continue;
                         if (rollingRocks > 0) {
-                            input[i][col] = ROLLING_STONE;
+                            rockField[i][col] = ROLLING_STONE;
                             rollingRocks--;
                         } else {
-                            input[i][col] = EMPTY;
+                            rockField[i][col] = EMPTY;
                         }
                     }
                     start = row;
@@ -46,33 +56,111 @@ public class ParabolicReflector {
         }
     }
 
-    private static void analyze(char[][] input) {
-        System.out.println();
+    private static void tiltSouth() {
+
+        for (int col = 0; col < width; col++) {
+            var start = 0;
+            int emptyNodes = 0;
+            for (int row = 0; row < height; row++) {
+                var curr = rockField[row][col];
+                if (curr == EMPTY) {
+                    emptyNodes++;
+                }
+                if (curr == SQUARE_STONE || row == height - 1) {
+                    for (int i = start; i <= row; i++) {
+                        if (rockField[i][col] == SQUARE_STONE) continue;
+                        if (emptyNodes > 0) {
+                            rockField[i][col] = EMPTY;
+                            emptyNodes--;
+                        } else {
+                            rockField[i][col] = ROLLING_STONE;
+                        }
+                    }
+                    start = row;
+                }
+            }
+        }
+    }
+
+    private static void tiltWest() {
+        for (int row = 0; row < height; row++) {
+            var start = 0;
+            int rollingRocks = 0;
+            for (int col = 0; col < width; col++) {
+                var curr = rockField[row][col];
+                if (curr == ROLLING_STONE) {
+                    rollingRocks++;
+                }
+                if (curr == SQUARE_STONE || col == width - 1) {
+                    for (int i = start; i <= col; i++) {
+                        if (rockField[row][i] == SQUARE_STONE) continue;
+                        if (rollingRocks > 0) {
+                            rockField[row][i] = ROLLING_STONE;
+                            rollingRocks--;
+                        } else {
+                            rockField[row][i] = EMPTY;
+                        }
+                    }
+                    start = col;
+                }
+            }
+        }
+    }
+
+    private static void tiltEast() {
+        for (int row = 0; row < height; row++) {
+            var start = 0;
+            int empties = 0;
+            for (int col = 0; col < width; col++) {
+                var curr = rockField[row][col];
+                if (curr == EMPTY) {
+                    empties++;
+                }
+                if (curr == SQUARE_STONE || col == width - 1) {
+                    for (int i = start; i <= col; i++) {
+                        if (rockField[row][i] == SQUARE_STONE) continue;
+                        if (empties > 0) {
+                            rockField[row][i] = EMPTY;
+                            empties--;
+                        } else {
+                            rockField[row][i] = ROLLING_STONE;
+                        }
+                    }
+                    start = col;
+                }
+            }
+        }
+    }
+
+
+    private static long analyze() {
+        if (log) System.out.println();
 
         int rollingStones = 0;
         int squareStones = 0;
         int emptySpots = 0;
-        int total = 0;
-        for (int i = 0; i < input.length; i++) {
+        long total = 0L;
+        for (int i = 0; i < rockField.length; i++) {
             int rs = 0;
-            for (int j = 0; j < input[0].length; j++) {
-                char c = input[i][j];
+            for (int j = 0; j < rockField[0].length; j++) {
+                char c = rockField[i][j];
                 if (c == 'O') rs++;
                 if (c == '#') squareStones++;
                 if (c == '.') emptySpots++;
-                System.out.print(c + " ");
+                if (log) System.out.print(c + " ");
             }
-            int multiplier = input.length - i;
+            int multiplier = rockField.length - i;
             int rowResult = rs * multiplier;
-            System.out.printf(" - %3d * %3d = %d%n", multiplier, rs, rowResult);
+            if (log) System.out.printf(" - %3d * %3d = %d%n", multiplier, rs, rowResult);
             total += rowResult;
             rollingStones += rs;
         }
-        System.out.println("TOTAL         : " + total);
-        System.out.println("Rolling stones: " + rollingStones);
-        System.out.println("Square stones : " + squareStones);
-        System.out.println("Empty spots   : " + emptySpots);
-        System.out.println();
+        if (log) System.out.println("TOTAL         : " + total);
+        if (log) System.out.println("Rolling stones: " + rollingStones);
+        if (log) System.out.println("Square stones : " + squareStones);
+        if (log) System.out.println("Empty spots   : " + emptySpots);
+        if (log) System.out.println();
+        return total;
     }
 
 }

56774abe3e0c43a48bcef5430b70fc146b318309
Rogier Roex <rogier.roex@gmail.com>
2023-12-28 20:30:33 +0100

    day 12

--- src/main/java/aoc/day12/Day12.java
+++ src/main/java/aoc/day12/Day12.java
@@ -6,16 +6,8 @@ import aoc.FileUtil;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class HotSpring {
-    public static final char OPERATIONAL_SPRING = '.';
-    public static final char UNKNOWN_SPRING = '?';
-    public static final char DAMAGED_SPRING = '#';
-
-    private final Map<ConditionRecord, Long> map;
-    private List<String> damagedConditionRecordsOfWhichSpringsAreDamaged;
-
-
-    public static void main(String[] args) {
+public class Day12 {
+    static void main(String[] args) {
         var hotSpring = new HotSpring(FileUtil.readFile("day12"));
 
         ChristmasAssert.test(hotSpring.repairDamagedConditionRecordsOfWhichSpringsAreDamaged(), 7716L);
@@ -23,13 +15,21 @@ public class HotSpring {
         hotSpring.unfoldDocument();
         ChristmasAssert.test(hotSpring.repairDamagedConditionRecordsOfWhichSpringsAreDamaged(), 18716325559999L);
     }
+}
 
-    public HotSpring(List<String> damagedConditionRecordsOfWhichSpringsAreDamaged) {
+class HotSpring {
+    private static final char OPERATIONAL_SPRING = '.';
+    private static final char UNKNOWN_SPRING = '?';
+    private static final char DAMAGED_SPRING = '#';
+    private List<String> damagedConditionRecordsOfWhichSpringsAreDamaged;
+    private final Map<ConditionRecord, Long> map;
+
+    HotSpring(List<String> damagedConditionRecordsOfWhichSpringsAreDamaged) {
         this.damagedConditionRecordsOfWhichSpringsAreDamaged = damagedConditionRecordsOfWhichSpringsAreDamaged;
         map = new HashMap<>();
     }
 
-    public long repairDamagedConditionRecordsOfWhichSpringsAreDamaged() {
+    long repairDamagedConditionRecordsOfWhichSpringsAreDamaged() {
         var t0 = System.currentTimeMillis();
         long sumOfHowBrokenTheseConditionRecordsReallyAre = 0L;
         for (var damagedConditionRecordOfWhichSpringsAreDamaged : damagedConditionRecordsOfWhichSpringsAreDamaged) {
@@ -38,18 +38,19 @@ public class HotSpring {
             var groups = Arrays.stream(splitRecord[1].split(",")).map(Long::parseLong).toList();
             sumOfHowBrokenTheseConditionRecordsReallyAre += countAllPossibleArrangements(springs, groups);
         }
-        System.out.println(STR. "Repair of damaged springs records of which springs are damaged result: \{ sumOfHowBrokenTheseConditionRecordsReallyAre } (\{ System.currentTimeMillis() - t0 }ms)" );
+        System.out.println(STR."Repair of damaged springs records of which springs are damaged result: \{sumOfHowBrokenTheseConditionRecordsReallyAre} (\{System.currentTimeMillis() - t0}ms)");
         return sumOfHowBrokenTheseConditionRecordsReallyAre;
     }
 
     /**
      * Utilizing Dynamic Programming / Recursive Memoization to make the enormous computation possible.
      * A recursive algorithm that maps conclusions so if it crosses it again, it can simply collect the result from the HashMap
+     *
      * @param springRecord The damaged condition record of damaged springs
-     * @param groups The contiguous groups
+     * @param groups       The contiguous groups
      * @return Number of different possible arrangements of OPERATIONAL and DAMAGED springs
      */
-    public long countAllPossibleArrangements(String springRecord, List<Long> groups) {
+    long countAllPossibleArrangements(String springRecord, List<Long> groups) {
         ConditionRecord conditionRecord = new ConditionRecord(springRecord, groups);
         if (map.containsKey(conditionRecord)) {
             // = recursive memoization i.e. already mapped, so dont compute again!
@@ -72,7 +73,7 @@ public class HotSpring {
             arrangements += countAllPossibleArrangements(DAMAGED_SPRING + springRecord.substring(1), groups);
         } else if (!groups.isEmpty()) {
             // For DAMAGED('#') springs, compute the correct signs to match the next GROUP
-            long nrDamaged = groups.get(0);
+            long nrDamaged = groups.getFirst();
             if (nrDamaged <= springRecord.length() && springRecord.chars().limit(nrDamaged).allMatch(c -> c == DAMAGED_SPRING || c == UNKNOWN_SPRING)) {
                 List<Long> newGroups = groups.subList(1, groups.size());
                 if (nrDamaged == springRecord.length()) {
@@ -88,7 +89,7 @@ public class HotSpring {
         return arrangements;
     }
 
-    public void unfoldDocument() {
+    void unfoldDocument() {
         System.out.println("Unfolding the document....");
         damagedConditionRecordsOfWhichSpringsAreDamaged = damagedConditionRecordsOfWhichSpringsAreDamaged.stream()
                 .map(s -> {
@@ -100,5 +101,6 @@ public class HotSpring {
                 .collect(Collectors.toList());
     }
 
-    record ConditionRecord(String springConditions, List<Long> groups) { }
 }
+
+record ConditionRecord(String springConditions, List<Long> groups) { }
